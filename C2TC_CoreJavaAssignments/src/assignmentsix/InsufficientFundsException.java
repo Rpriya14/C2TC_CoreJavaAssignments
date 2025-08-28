7bad7a31a620968adf05e408902d45b60538dfe5
Hp <Hp@LAPTOP-07P0GB5R>
2025-08-28 15:32:30 +0530

    Initial commit

---
+++ C2TC_CoreJavaAssignments/src/assignmentsix/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package assignmentsix;
+
+//InsufficientFundsException.java
+public class InsufficientFundsException extends Exception {
+ public InsufficientFundsException(String message) {
+     super(message);
+ }
+}
